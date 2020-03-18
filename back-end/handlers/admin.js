dbd4b8b6096815a37af244a42aef3904559f0c04
psyCOSMO <max.funoff@gmail.com>
2020-03-18 13:15:10 +0200

    Added admin routes, for future admin panel in front end

---
+++ back-end/handlers/admin.js
@@ -0,0 +1,30 @@
+const pool = require('../mysql/dbpool')
+
+const createVacation = async (req, res) => {
+    let response = {
+        success: false,
+    }
+    let code = 401;
+
+    const fileLocation = req.file ? 'http://localhost:3000/uploads/' + req.file.filename : null;
+    const vacation = [req.body.name, req.body.description, req.body.StartDate, req.body.EndDate, req.body.price, fileLocation];
+
+    const query =
+        `INSERT INTO vacations(name, description, start_date, end_date, price, image) 
+        VALUES (?, ?, ?, ?, ?, ?)`
+
+    try {
+        await pool.execute(query, vacation)
+
+        response.data = 'Vacation created succesfully';
+        response.success = true;
+        code = 201;
+    }
+    catch (err) {
+        response.err = err;
+        code = 500;
+    }
+
+    res.status(code).json(response)
+
+}
