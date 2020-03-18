2306e0765f58b9307b15a7e747ec4bc08c78f6cf
psyCOSMO <max.funoff@gmail.com>
2020-03-18 13:20:29 +0200

    Removed unneeded code that was moved to a different route

--- back-end/handlers/vacations.js
+++ back-end/handlers/vacations.js
@@ -55,34 +55,6 @@ const byID = async (req, res) => {
     res.status(code).json(response)
 }
 
-const createVacation = async (req, res) => {
-    let response = {
-        success: false,
-    }
-    let code = 401;
-
-    const fileLocation = req.file ? 'http://localhost:3000/uploads/' + req.file.filename : null;
-    const vacation = [req.body.name, req.body.description, req.body.StartDate, req.body.EndDate, req.body.price, fileLocation];
-
-    const query =
-        `INSERT INTO vacations(name, description, start_date, end_date, price, image) 
-        VALUES (?, ?, ?, ?, ?, ?)`
-
-    try {
-        await pool.execute(query, vacation)
-
-        response.data = 'Vacation created succesfully';
-        response.success = true;
-        code = 201;
-    }
-    catch (err) {
-        response.err = err;
-        code = 500;
-    }
-
-    res.status(code).json(response)
-
-}
 
 const addVacationToUser = async (req, res) => {
     let response = {
@@ -180,7 +152,6 @@ const cancelRefundVacation = async (req, res) => {
 module.exports = {
     allVacations,
     byID,
-    createVacation,
     addVacationToUser,
     refundVacationFromUser,
     cancelRefundVacation,
--- back-end/routes/vacations.js
+++ back-end/routes/vacations.js
@@ -3,27 +3,6 @@ const router = express.Router();
 const handler = require('../handlers/vacations')
 const auth = require('../auth/jwtAuth')
 const authAdmin = require('../auth/jwtAuthAdmin')
-const multer = require('multer')
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + file.originalname)
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
-        cb(null, true)
-    else
-        cb({status: 422, message: 'Wrong file type'}, false)
-}
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
-})
 
 
 /* GET vacations listing. */
@@ -31,10 +10,7 @@ router.get('/', handler.allVacations);
 router.get('/:id', handler.byID);
 
 /* POST vacations listing */
-router.post('/', authAdmin, upload.single('image'), handler.createVacation)
 router.post('/:id/add', auth, handler.addVacationToUser)
-
-
 
 /* PUT vacations listing */
 router.put('/:id/refund', auth, handler.refundVacationFromUser)
