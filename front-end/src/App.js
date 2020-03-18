aa6275b9978d679037478e0c3da2c28207b6eb84
psyCOSMO <max.funoff@gmail.com>
2020-03-18 14:29:11 +0200

    Added placeholder pages and a placeholder navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home/Home'
+import Connect from './pages/Connect/Connect'
+import Navbar from './components/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Router>
 
+        <nav>
+          <Navbar />
+        </nav>
+
+        <main>
+          <Switch>
+
+            <Route path="/connect" component={Connect}/>
+            
+            <Route path="/" exact={true} component={Home} />
+
+          </Switch>
+        </main>
+        
+      </Router>
     </div>
   );
 }
---
+++ front-end/src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+
+class Home extends React.Component {
+
+
+    render(){
+        return(
+            <div className='home-p'>
+
+                This is Home Page
+
+            </div>
+        )
+    }
+}
+
+export default Home
