87a310f0acb4f957bda2f63719168a386e16390f
psyCOSMO <max.funoff@gmail.com>
2020-03-26 01:41:36 +0200

    Added additional middleware that allows cookie reading and setting

--- back-end/app.js
+++ back-end/app.js
@@ -11,12 +11,20 @@ const adminRouter = require('./routes/admin');
 
 const app = express();
 
+
 /*Cors Config When Production*/
-app.use(cors())
+app.use(cors({origin: 'http://localhost:3000', credentials: true}))
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use((req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/uploads', express.static('uploads'));
 app.use('/users', usersRouter);
